5c399c761b37c22b4f03abd853a516542ff197fa
Abideen Bamgbala <abam1993@hotmail.com>
2015-02-05 13:48:11 +0100

    Bug im Controller behoben

--- src/main/java/at/skobamg/generator/view/SwitchtypController.java
+++ src/main/java/at/skobamg/generator/view/SwitchtypController.java
@@ -5,8 +5,11 @@ package at.skobamg.generator.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import at.skobamg.generator.mediator.IEventMediator;
 
 /**
@@ -17,7 +20,7 @@ public class SwitchtypController extends ScreensAbstract {
 	@Autowired
 	private IEventMediator mediator;
 	@FXML
-	private TextField switchname;
+	private ComboBox<String> switchname;
 	@FXML
 	private TextField iosversion;
 
@@ -26,10 +29,10 @@ public class SwitchtypController extends ScreensAbstract {
 	}
 	
 	public void neuenSwitchtyp(){
-		if(switchname.getText().isEmpty() || iosversion.getText().isEmpty())
+		if(switchname.getSelectionModel().getSelectedItem().isEmpty() || iosversion.getText().isEmpty())
 			mediator.nachrichtAnzeigen("Bitte geben Sie alle erforderlichen Werte ein");
 		else{
-			if(mediator.neuenSwitchtyp(switchname.getText(), iosversion.getText())){
+			if(mediator.neuenSwitchtyp(switchname.getSelectionModel().getSelectedItem(), iosversion.getText())){
 				mediator.nachrichtAnzeigen("Neuer Switchtyp wurde erfolgreich hinzugefügt");
 				mediator.zumHauptfenster();
 			}
